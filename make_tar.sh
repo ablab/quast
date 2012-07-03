52899219c8163e1d0f1d0a4be64e55a8fe28af11
Vladislav Saveliev <vladislav.sav@gmail.com>
2012-07-03 16:29:21 +0400

    make_tar makes release version

--- make_tar.sh
+++ make_tar.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+mkdir release
 mkdir release/quast
 cp -r libs            release/quast
-cp release/quality.py release/quast
-#cp release/README     release/quast    # README is for developers and it is outdated -- users should use only manual
 cp quast.py           release/quast
 cp manual.html        release/quast
+sed "s/RELEASE_MODE=False/RELEASE_MODE=True/" quast.py > release/quast/quast.py
 
-make -C release/quast/libs/MUMmer3.23-osx/ clean >/dev/null 2>/dev/null
+make -C release/quast/libs/MUMmer3.23-osx/   clean >/dev/null 2>/dev/null
 make -C release/quast/libs/MUMmer3.23-linux/ clean >/dev/null 2>/dev/null
 rm -f   release/quast/libs/*.pyc
-rm -rf  release/quast/results_* 
+rm -rf  release/quast/results_*
 rm -rf	release/quast/libs/mauve
 rm -rf	release/quast/libs/genemark_suite_linux_64
 
@@ -17,5 +17,5 @@ cd release
 tar -pczf quast.tar.gz quast
 cd ..
 mv release/quast.tar.gz .
-rm -rf release/quast
-echo "QUAST archive created: quast.tar.gz"
+rm -rf release
+echo "Quast archive created: quast.tar.gz"

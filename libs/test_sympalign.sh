17ff3a7c18aedc1f7a2b24902d37320c0297d541
Alexey Gurevich <alexeigurevich@gmail.com>
2012-08-14 13:30:20 +0400

    remove stuff from libs and modify MUMmer OSX makefile to fix compilation http://ablab.myjetbrains.com/youtrack/issue/QUAST-97

--- libs/test_sympalign.sh
+++
@@ -1,4 +0,0 @@
-#python sympalign.py 1 outsymp.coords results/plantakolya/nucmer_contigs_final.fasta.coords.btab results/plantakolya/nucmer_velvet_contigs.fa.coords.btab
-#python sympalign.py 2 outsymp.segments results/plantakolya/nucmer_contigs_final.fasta.coords.btab results/plantakolya/nucmer_velvet_contigs.fa.coords.btab
-python sympalign.py 2 ../results/plantakolya/out.segments ../results/plantakolya/
-#*.btab

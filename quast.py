ed413fb5bfa519f62c57a16630dfddee576254af
Alexey Gurevich <alexeigurevich@gmail.com>
2012-09-05 16:00:02 +0400

    Fix bug in: Add info about min contig to HTML report

--- quast.py
+++ quast.py
@@ -319,7 +319,7 @@ def main(args, lib_dir=os.path.join(__location__, 'libs')): # os.path.join(os.pa
     reporting.save(output_dirpath, qconfig.min_contig)
 
     if json_outputpath:
-        json_saver.save_total_report(json_outputpath, min_contig)
+        json_saver.save_total_report(json_outputpath, qconfig.min_contig)
 
     from libs.html_saver import html_saver
     html_saver.save_total_report(output_dirpath, qconfig.min_contig)

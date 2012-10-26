7aea919893cbf3bf21c0fa941e25fb612ea95c84
Alexey Gurevich <alexeigurevich@gmail.com>
2012-10-26 16:18:54 +0400

    fixed bug in other_scripts/get_contig_by_id

--- other_scripts/get_contig_by_id.py
+++ other_scripts/get_contig_by_id.py
@@ -10,6 +10,9 @@ sys.path.append(os.path.join(os.path.abspath(sys.path[0]), '../libs'))
 
 import fastaparser
 
+def get_corr_name(name):
+    return re.sub(r'\W', '', re.sub(r'\s', '_', name))
+
 # MAIN
 if len(sys.argv) != 3:
 	print("Usage: " + sys.argv[0] + " <input fasta> <contig id or file with list of contig ids>")	
@@ -26,12 +29,13 @@ origin_fasta = fastaparser.read_fasta(sys.argv[1])
 dict_of_all_contigs = dict()
 selected_contigs = []
 for (name, seq) in origin_fasta:
-    corr_name = re.sub(r'\W', '', re.sub(r'\s', '_', name))
+    corr_name = get_corr_name(name)
     dict_of_all_contigs[corr_name] = seq
 
 for name in list_of_ids:
-    if name in dict_of_all_contigs:
-        selected_contigs.append((name, dict_of_all_contigs[name]))
+    corr_name = get_corr_name(name)
+    if corr_name in dict_of_all_contigs:
+        selected_contigs.append((name, dict_of_all_contigs[corr_name]))
     else:
         print >> sys.stderr, "Contig", name, "(cor name:", corr_name, ") not found!"
 

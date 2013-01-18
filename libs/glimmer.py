961bf37082e550a4dbedd220daca90cb02d934fe
Alexey Gurevich <alexeigurevich@gmail.com>
2013-01-18 18:41:50 +0300

    Standartazing output and making gene prediction in parallel

--- libs/glimmer.py
+++ libs/glimmer.py
@@ -8,13 +8,13 @@ from __future__ import with_statement
 
 import os
 import tempfile
-import platform
 import subprocess
 import itertools
 import csv
 import shutil
 
 from libs import reporting
+from libs import qconfig
 from libs.fastaparser import read_fasta, write_fasta, rev_comp
 from qutils import id_to_str, print_timestamp, error
 
@@ -69,9 +69,9 @@ def glimmerHMM(tool_dir, fasta_path, out_path, gene_lengths, err_path):
         contigs[id] = seq
 
     out_gff_path = merge_gffs(gffs, out_path + '_genes.gff')
-    out_fasta_path = out_path + '_genes.fasta'
+    #out_fasta_path = out_path + '_genes.fasta'
     unique, total = set(), 0
-    genes = []
+    #genes = []
     cnt = [0] * len(gene_lengths)
     for contig, gene_id, start, end, strand in parse_gff(out_gff_path):
         total += 1
@@ -84,15 +84,38 @@ def glimmerHMM(tool_dir, fasta_path, out_path, gene_lengths, err_path):
         if gene_seq not in unique:
             unique.add(gene_seq)
 
-        genes.append((gene_id, gene_seq))
+        #genes.append((gene_id, gene_seq))
 
         for idx, gene_length in enumerate(gene_lengths):
             cnt[idx] += end - start > gene_length
 
-    write_fasta(out_fasta_path, genes)
+    #write_fasta(out_fasta_path, genes)
     shutil.rmtree(base_dir)
 
-    return out_gff_path, out_fasta_path, len(unique), total, cnt
+    #return out_gff_path, out_fasta_path, len(unique), total, cnt
+    return out_gff_path, len(unique), total, cnt
+
+
+def predict_genes(id, fasta_path, gene_lengths, out_dir, tool_dir):
+    report = reporting.get(fasta_path)
+
+    print '  ' + id_to_str(id) + os.path.basename(fasta_path)
+
+    out_name = os.path.basename(fasta_path)
+    out_path = os.path.join(out_dir, out_name)
+    err_path = os.path.join(out_dir, out_name + '_glimmer.stderr')
+    #out_gff_path, out_fasta_path, unique, total, cnt = glimmerHMM(tool_dir,
+    #    fasta_path, out_path, gene_lengths, err_path)
+    out_gff_path, unique, total, cnt = glimmerHMM(tool_dir,
+        fasta_path, out_path, gene_lengths, err_path)
+    print '  ' + id_to_str(id) + '  Genes = ' + str(unique) + ' unique, ' + str(total) + 'total'
+    print '  ' + id_to_str(id) + '  Predicted genes (GFF): ' + out_gff_path
+
+    report.add_field(reporting.Fields.GENEMARKUNIQUE, unique)
+    report.add_field(reporting.Fields.GENEMARK, cnt)
+
+    print '  ' + id_to_str(id) + 'Gene prediction is finished.'
+
 
 def do(fasta_paths, gene_lengths, out_dir, lib_dir):
     print_timestamp()
@@ -117,29 +140,38 @@ def do(fasta_paths, gene_lengths, out_dir, lib_dir):
     if not os.path.isdir(out_dir):
         os.makedirs(out_dir)
 
-    for id, fasta_path in enumerate(fasta_paths):
-        report = reporting.get(fasta_path)
+    n_jobs = min(len(fasta_paths), qconfig.max_threads)
+    from joblib import Parallel, delayed
+    Parallel(n_jobs=n_jobs)(delayed(predict_genes)(id, fasta_path, gene_lengths, out_dir, tool_dir)
+        for id, fasta_path in enumerate(fasta_paths))
 
-        print ' ', id_to_str(id), os.path.basename(fasta_path),
+    print '  Done.'
 
-        out_name = os.path.basename(fasta_path)
-        out_path = os.path.join(out_dir, out_name)
-        err_path = os.path.join(out_dir, 'glimmer.errors')
-        out_gff_path, out_fasta_path, unique, total, cnt = glimmerHMM(tool_dir,
-            fasta_path, out_path, gene_lengths, err_path)
-
-        print(', Genes = %i unique, %i total' % (unique, total))
-        print('    Glimmer output: %s and %s' % (out_gff_path, out_fasta_path))
-
-        report.add_field(reporting.Fields.GENEMARKUNIQUE, unique)
-        report.add_field(reporting.Fields.GENEMARK, cnt)
-
-        print '  Done'
+#    for id, fasta_path in enumerate(fasta_paths):
+#        report = reporting.get(fasta_path)
+#
+#        print ' ', id_to_str(id), os.path.basename(fasta_path),
+#
+#        out_name = os.path.basename(fasta_path)
+#        out_path = os.path.join(out_dir, out_name)
+#        err_path = os.path.join(out_dir, 'glimmer.errors')
+#        #out_gff_path, out_fasta_path, unique, total, cnt = glimmerHMM(tool_dir,
+#        #    fasta_path, out_path, gene_lengths, err_path)
+#        out_gff_path, unique, total, cnt = glimmerHMM(tool_dir,
+#            fasta_path, out_path, gene_lengths, err_path)
+#
+#        print(', Genes = %i unique, %i total' % (unique, total))
+#        print('    Glimmer output: %s' % (out_gff_path))
+#
+#        report.add_field(reporting.Fields.GENEMARKUNIQUE, unique)
+#        report.add_field(reporting.Fields.GENEMARK, cnt)
+#
+#        print '  Done'
 
 if __name__ == '__main__':
-    fasta_pathes = ['../test_data/assembly_10K_1.fasta', '../test_data/assembly_10K_2.fasta']
-    print fasta_pathes
+    fasta_paths = ['../test_data/assembly_10K_1.fasta', '../test_data/assembly_10K_2.fasta']
+    print fasta_paths
     out_dir = '../run_test_data/glimmer_out'
     lib_dir = ''
     gene_lengths = [100, 1000, 10000]
-    do(fasta_pathes, gene_lengths, out_dir, lib_dir)
+    do(fasta_paths, gene_lengths, out_dir, lib_dir)

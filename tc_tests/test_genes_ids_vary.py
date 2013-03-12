a2ae0f2887af27e4e0df0f59753ada6f68215ca5
vladsaveliev <vladsaveliev@me.com>
2013-03-12 18:33:27 +0400

    fixed genes

--- tc_tests/test_genes_ids_vary.py
+++ tc_tests/test_genes_ids_vary.py
@@ -6,6 +6,6 @@ from common import run_quast, contigs_2_1k, reference_1k, assert_metric
 name = os.path.basename(__file__)[5:-3]
 
 
-run_quast(name, contigs=[contigs_2_1k], params='-R ' + reference_1k + ' -G genes_1k__ids_differ.txt')
+run_quast(name, contigs=[contigs_2_1k], params='-R ' + reference_1k + ' -G genes_1k_ids_differ.txt')
 assert_metric(name, '# genes', ['0 + 1 part'])
 

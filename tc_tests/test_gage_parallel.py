24c7c6ad7fe99513df759929efe468c4e72712aa
vladsaveliev <vladsaveliev@me.com>
2013-03-14 20:14:45 +0400

    gage parallel test

--- tc_tests/test_gage_parallel.py
+++ tc_tests/test_gage_parallel.py
@@ -7,7 +7,7 @@ from common import *
 name = os.path.basename(__file__)[5:-3]
 
 
-run_quast(name, contigs=[contigs_1k_1, contigs_1k_2], params='--gage -R ' + reference_1k + ' --threads 2')
+run_quast(name, contigs=[contigs_1k_1, contigs_1k_2], params='--gage -R reference_1k.fa.gz --threads 2')
 
 check_report_files(name, ['gage_report.txt',
                           'gage_report.tsv',
@@ -16,4 +16,4 @@ check_report_files(name, ['gage_report.txt',
                           'gage_transposed_report.txt',
                           'gage_transposed_report.tsv'])
 
-assert_metric(name, 'Assembly size', ['760'], 'gage_report.tsv')
+assert_metric(name, 'Assembly size', ['1000', '760'], 'gage_report.tsv')

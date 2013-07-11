aec1fe90acd90550bacbdd97a3164b71f9a9fd85
vladsaveliev <vladsaveliev@me.com>
2013-07-11 21:53:20 +0400

    usage labels correction

--- quast.py
+++ quast.py
@@ -44,58 +44,58 @@ def _usage():
         print >> sys.stderr, "--min-contig  <int>          Lower threshold for contig length [default: %s]" % qconfig.min_contig
         print >> sys.stderr, ""
         print >> sys.stderr, "Advanced options:"
-        print >> sys.stderr, "-t  --threads <int>                 Maximum number of threads [default: number of CPUs]"
-        print >> sys.stderr, "-l  --labels <\"label, label, ...\">  Names of assemblies to use in reports, comma-separated."
-        print >> sys.stderr, "--gage                              Starts GAGE inside QUAST (\"GAGE mode\")"
-        print >> sys.stderr, "--contig-thresholds <int,int,..>    Comma-separated list of contig length thresholds [default: %s]" % qconfig.contig_thresholds
-        print >> sys.stderr, "--gene-finding                      Uses Gene Finding module"
-        print >> sys.stderr, "--gene-thresholds <int,int,..>      Comma-separated list of threshold lengths of genes to search with Gene Finding module"
-        print >> sys.stderr, "                                    [default is %s]" % qconfig.genes_lengths
-        print >> sys.stderr, "--eukaryote                         Genome is an eukaryote"
-        print >> sys.stderr, "--est-ref-size <int>                Estimated reference size (for computing NGx metrics without a reference)"
-        print >> sys.stderr, "--scaffolds                         Provided assemblies are scaffolds"
-        print >> sys.stderr, "--use-all-alignments                Computes Genome fraction, # genes, # operons metrics in compatible with QUAST v.1.* mode."
-        print >> sys.stderr, "                                    By default, QUAST filters Nucmer\'s alignments to keep only best ones"
-        print >> sys.stderr, "--ambiguity-usage <none|one|all>    Uses none, one, or all alignments of a contig with multiple equally good alignments."
-        print >> sys.stderr, "                                    [default is %s]" % qconfig.ambiguity_usage
-        print >> sys.stderr, "--strict-NA                         Breaks contigs by any misassembly event to compute NAx and NGAx."
-        print >> sys.stderr, "                                    By default, QUAST breaks contigs only by extensive misassemblies (not local ones)"
-        print >> sys.stderr, "-m  --meta                          Metagenomic assembly. Uses MetaGeneMark for gene prediction. "
+        print >> sys.stderr, "-t  --threads <int>               Maximum number of threads [default: number of CPUs]"
+        print >> sys.stderr, "-l  --labels \"label, label, ...\"  Names of assemblies to use in reports, comma-separated."
+        print >> sys.stderr, "--gage                            Starts GAGE inside QUAST (\"GAGE mode\")"
+        print >> sys.stderr, "--contig-thresholds <int,int,..>  Comma-separated list of contig length thresholds [default: %s]" % qconfig.contig_thresholds
+        print >> sys.stderr, "--gene-finding                    Uses Gene Finding module"
+        print >> sys.stderr, "--gene-thresholds <int,int,..>    Comma-separated list of threshold lengths of genes to search with Gene Finding module"
+        print >> sys.stderr, "                                  [default is %s]" % qconfig.genes_lengths
+        print >> sys.stderr, "--eukaryote                       Genome is an eukaryote"
+        print >> sys.stderr, "--est-ref-size <int>              Estimated reference size (for computing NGx metrics without a reference)"
+        print >> sys.stderr, "--scaffolds                       Provided assemblies are scaffolds"
+        print >> sys.stderr, "--use-all-alignments              Computes Genome fraction, # genes, # operons metrics in compatible with QUAST v.1.* mode."
+        print >> sys.stderr, "                                  By default, QUAST filters Nucmer\'s alignments to keep only best ones"
+        print >> sys.stderr, "--ambiguity-usage <none|one|all>  Uses none, one, or all alignments of a contig with multiple equally good alignments."
+        print >> sys.stderr, "                                  [default is %s]" % qconfig.ambiguity_usage
+        print >> sys.stderr, "--strict-NA                       Breaks contigs by any misassembly event to compute NAx and NGAx."
+        print >> sys.stderr, "                                  By default, QUAST breaks contigs only by extensive misassemblies (not local ones)"
+        print >> sys.stderr, "-m  --meta                        Metagenomic assembly. Uses MetaGeneMark for gene prediction. "
         print >> sys.stderr, ""
-        print >> sys.stderr, "--test                              Runs QUAST with the data in the test_data folder."
-        print >> sys.stderr, "-h  --help                          Prints this message"
+        print >> sys.stderr, "--test                            Runs QUAST with the data in the test_data folder."
+        print >> sys.stderr, "-h  --help                        Prints this message"
 
     else:
         print >> sys.stderr, 'Options with arguments'
-        print >> sys.stderr, "-o  --output-dir   <dirname>        Directory to store all result files [default: quast_results/results_<datetime>]"
-        print >> sys.stderr, "-R                 <filename>       Reference genome file"
-        print >> sys.stderr, "-G  --genes        <filename>       Annotated genes file"
-        print >> sys.stderr, "-O  --operons      <filename>       Annotated operons file"
-        print >> sys.stderr, "-M  --min-contig   <int>            Lower threshold for contig length [default: %s]" % qconfig.min_contig
-        print >> sys.stderr, "-t  --contig-thresholds             Comma-separated list of contig length thresholds [default: %s]" % qconfig.contig_thresholds
-        print >> sys.stderr, "-S  --gene-thresholds               Comma-separated list of threshold lengths of genes to search with Gene Finding module [default: %s]" % qconfig.genes_lengths
-        print >> sys.stderr, "-T  --threads      <int>            Maximum number of threads [default: number of CPUs]"
-        print >> sys.stderr, "-l  --labels <\"label, label, ...\">  Names of assemblies to use in reports, comma-separated."
-        print >> sys.stderr, "-c  --mincluster   <int>            Nucmer's parameter -- the minimum length of a cluster of matches [default: %s]" % qconfig.mincluster
-        print >> sys.stderr, "    --est-ref-size <int>            Estimated reference size (for calculating NG)"
+        print >> sys.stderr, "-o  --output-dir   <dirname>      Directory to store all result files [default: quast_results/results_<datetime>]"
+        print >> sys.stderr, "-R                 <filename>     Reference genome file"
+        print >> sys.stderr, "-G  --genes        <filename>     Annotated genes file"
+        print >> sys.stderr, "-O  --operons      <filename>     Annotated operons file"
+        print >> sys.stderr, "-M  --min-contig   <int>          Lower threshold for contig length [default: %s]" % qconfig.min_contig
+        print >> sys.stderr, "-t  --contig-thresholds           Comma-separated list of contig length thresholds [default: %s]" % qconfig.contig_thresholds
+        print >> sys.stderr, "-S  --gene-thresholds             Comma-separated list of threshold lengths of genes to search with Gene Finding module [default: %s]" % qconfig.genes_lengths
+        print >> sys.stderr, "-T  --threads      <int>          Maximum number of threads [default: number of CPUs]"
+        print >> sys.stderr, "-l  --labels \"label, label, ...\"  Names of assemblies to use in reports, comma-separated."
+        print >> sys.stderr, "-c  --mincluster   <int>          Nucmer's parameter -- the minimum length of a cluster of matches [default: %s]" % qconfig.mincluster
+        print >> sys.stderr, "    --est-ref-size <int>          Estimated reference size (for calculating NG)"
         print >> sys.stderr, ""
         print >> sys.stderr, "Options without arguments"
-        print >> sys.stderr, "-f  --gene-finding                  Uses Gene Finding module"
-        print >> sys.stderr, "-s  --scaffolds                     This flag informs QUAST that provided assemblies are scaffolds"
-        print >> sys.stderr, "    --gage                          Uses GAGE (results are in gage_report.txt)"
-        print >> sys.stderr, "-e  --eukaryote                     Genome is eukaryotic"
-        print >> sys.stderr, "-m  --meta                          Metagenomic assembly. Uses MetaGeneMark for gene prediction. "
-        print >> sys.stderr, "-a  --ambiguity-usage <str>         Uses all alignments of contigs covering repeats (ambiguous). Str is none|one|all"
-        print >> sys.stderr, "-u  --use-all-alignments            Computes Genome fraction, # genes, # operons in v.1.0-1.3 style"
-        print >> sys.stderr, "-n  --strict-NA                     Breaks contigs by any misassembly event to compute NAx and NGAx."
-        print >> sys.stderr, "-j  --save-json                     Saves the output also in the JSON format"
-        print >> sys.stderr, "-J  --save-json-to <path>           Saves the JSON-output to a particular path"
-        print >> sys.stderr, "    --no-html                       Doesn't build html report"
-        print >> sys.stderr, "    --no-plots                      Doesn't draw plots (to make quast faster)"
+        print >> sys.stderr, "-f  --gene-finding                Uses Gene Finding module"
+        print >> sys.stderr, "-s  --scaffolds                   This flag informs QUAST that provided assemblies are scaffolds"
+        print >> sys.stderr, "    --gage                        Uses GAGE (results are in gage_report.txt)"
+        print >> sys.stderr, "-e  --eukaryote                   Genome is eukaryotic"
+        print >> sys.stderr, "-m  --meta                        Metagenomic assembly. Uses MetaGeneMark for gene prediction. "
+        print >> sys.stderr, "-a  --ambiguity-usage <str>       Uses all alignments of contigs covering repeats (ambiguous). Str is none|one|all"
+        print >> sys.stderr, "-u  --use-all-alignments          Computes Genome fraction, # genes, # operons in v.1.0-1.3 style"
+        print >> sys.stderr, "-n  --strict-NA                   Breaks contigs by any misassembly event to compute NAx and NGAx."
+        print >> sys.stderr, "-j  --save-json                   Saves the output also in the JSON format"
+        print >> sys.stderr, "-J  --save-json-to <path>         Saves the JSON-output to a particular path"
+        print >> sys.stderr, "    --no-html                     Doesn't build html report"
+        print >> sys.stderr, "    --no-plots                    Doesn't draw plots (to make quast faster)"
         print >> sys.stderr, ""
-        print >> sys.stderr, "-d  --debug                         Runs in debug mode"
-        print >> sys.stderr, "--test                              Runs QUAST with the data in the test_data folder."
-        print >> sys.stderr, "-h  --help                          Prints this message"
+        print >> sys.stderr, "-d  --debug                       Runs in debug mode"
+        print >> sys.stderr, "--test                            Runs QUAST with the data in the test_data folder."
+        print >> sys.stderr, "-h  --help                        Prints this message"
 
 
 def _set_up_output_dir(output_dirpath, json_outputpath,

920f07dbc65edc63dfc8a2ea43c5221b589ccefb
Alexey Gurevich <alexeigurevich@gmail.com>
2013-09-05 15:08:18 +0400

    Fixed bug with circular CONTIGS

--- libs/genome_analyzer.py
+++ libs/genome_analyzer.py
@@ -208,6 +208,8 @@ def do(ref_fpath, contigs_fpaths, nucmer_dir, output_dirpath, genes_fpath, opero
                 break
             s1 = int(line.split('|')[0].split()[0])
             e1 = int(line.split('|')[0].split()[1])
+            s2 = int(line.split('|')[1].split()[0])
+            e2 = int(line.split('|')[1].split()[1])
             contig_name = line.split()[12].strip()
             chr_name = line.split()[11].strip()
             if chr_name not in genome_mapping:
@@ -215,13 +217,13 @@ def do(ref_fpath, contigs_fpaths, nucmer_dir, output_dirpath, genes_fpath, opero
                       "differ from the names in the reference. Try to remove the file and restart QUAST.")
                 continue
             aligned_blocks[contig_name].append(AlignedBlock(seqname=chr_name, start=s1, end=e1))
-            if s1 <= e1:
-                for i in range(s1, e1 + 1):
-                    genome_mapping[chr_name][i] = 1
-            else:  # circular genome, contig starts on the end of a chromosome and ends in the beginning
+            if s2 == 0 and e2 == 0:  # special case: circular genome, contig starts on the end of a chromosome and ends in the beginning
                 for i in range(s1, len(genome_mapping[chr_name])):
                     genome_mapping[chr_name][i] = 1
                 for i in range(1, e1 + 1):
+                    genome_mapping[chr_name][i] = 1
+            else: #if s1 <= e1:
+                for i in range(s1, e1 + 1):
                     genome_mapping[chr_name][i] = 1
         coordfile.close()
 

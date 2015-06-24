6868b12e646195ca5da4df72e5ed4b746161bcc9
almiheenko <al.miheenko@gmail.com>
2015-06-24 16:31:57 +0300

    added install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo 'Starting QUAST...'
+$(dirname '$0')/quast.py --test
+return_code=$?
+if [ $return_code -ne 0 ]; then
+   echo 'ERROR! QUAST TEST FAILED!'
+fi
+echo 'Starting MetaQUAST...'
+$(dirname "$0")/metaquast.py --test --download
+return_code=$?
+if [ $return_code -ne 0 ]; then
+   echo 'ERROR! METAQUAST TEST FAILED!'
+fi
+echo 'QUAST INSTALLED SUCCESSFULLY!'

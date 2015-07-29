4cba9259b1a342acc68b2e5b5c9c1721f14345df
almiheenko <al.miheenko@gmail.com>
2015-07-29 15:11:41 +0300

    MetaQUAST: added full_install.sh

--- full_install.sh
+++ full_install.sh
@@ -21,5 +21,14 @@ if [ $return_code -ne 0 ]; then
    echo 'ERROR! METAQUAST TEST FAILED!'
    exit 1
 fi
+echo "Starting MetaQUAST test without references... (stdout redirected to $stdout_log_fname)"
+echo "" >> $stdout_log_fname
+echo "Starting MetaQUAST test without references" >> $stdout_log_fname
+$quast_home/metaquast.py --test-no-ref --fast >> $stdout_log_fname
+return_code=$?
+if [ $return_code -ne 0 ]; then
+   echo 'ERROR! METAQUAST TEST WITHOUT REFERENCES FAILED!'
+   exit 1
+fi
 echo 'QUAST INSTALLED SUCCESSFULLY!'
 exit 0

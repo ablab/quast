3348eff5cf194eb63f6eaa45f76dc83a2b20e938
Alexey Gurevich <alexeigurevich@gmail.com>
2015-07-31 19:14:44 +0300

    Copyrights update

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+############################################################################
+# Copyright (c) 2015 Saint Petersburg State University
+# Copyright (c) 2011-2015 Saint Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
 # installs general QUAST pipeline and general MetaQUAST pipeline. 
 # MetaQUAST for de novo datasets (without references) will NOT be installed.
 
--- install_full.sh
+++ install_full.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+############################################################################
+# Copyright (c) 2015 Saint Petersburg State University
+# Copyright (c) 2011-2015 Saint Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
 # installs general QUAST pipeline, general MetaQUAST pipeline,
 # and MetaQUAST for de novo datasets (without references)
 

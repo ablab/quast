5ecaec46209138a74d7ecb89f6d8e1f2670030f5
Alexey Gurevich <alexeigurevich@gmail.com>
2016-03-24 20:21:18 +0300

    Full ecoli test added

---
+++ tc_tests/test_full_ecoli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import os
+from common import *
+
+name = os.path.basename(__file__)[5:-3]
+contigs = ['ecoli/%s.fasta.gz' % i for i in ['ABySS', 'EULER-SR', 'SPAdes', 'Velvet']]
+
+
+run_quast(name, contigs=contigs, params='-R reference.fa -G genes.txt -t 2')
+check_report_files(name)
+assert_report_header(name, contigs=contigs)
+assert_metric(name, 'N50', ['76520', '29342', '109140', '32469'])
+assert_metric(name, '# misassemblies', ['6', '15', '1', '2'])
+assert_metric(name, '# local misassemblies', ['1', '40', '8', '1'])
+assert_metric(name, '# genes', ['3704 + 121 part', '3411 + 308 part', '4029 + 67 part', '3079 + 136 part'])

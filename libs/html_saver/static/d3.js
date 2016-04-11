2bfce6fd5caef7e5634beb5a81676d870aad4c6e
almiheenko <al.miheenko@gmail.com>
2016-04-11 17:10:57 +0300

    Icarus: some IE fixes

--- libs/html_saver/static/d3.js
+++ libs/html_saver/static/d3.js
@@ -1,4 +1,188 @@
 (function() {
+
+    if (!Array.prototype.map) {
+
+      Array.prototype.map = function(callback, thisArg) {
+
+        var T, A, k;
+
+        if (this == null) {
+          throw new TypeError(' this is null or not defined');
+        }
+
+        // 1. Let O be the result of calling ToObject passing the |this|
+        //    value as the argument.
+        var O = Object(this);
+
+        // 2. Let lenValue be the result of calling the Get internal
+        //    method of O with the argument "length".
+        // 3. Let len be ToUint32(lenValue).
+        var len = O.length >>> 0;
+
+        // 4. If IsCallable(callback) is false, throw a TypeError exception.
+        // See: http://es5.github.com/#x9.11
+        if (typeof callback !== 'function') {
+          throw new TypeError(callback + ' is not a function');
+        }
+
+        // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+        if (arguments.length > 1) {
+          T = thisArg;
+        }
+
+        // 6. Let A be a new array created as if by the expression new Array(len)
+        //    where Array is the standard built-in constructor with that name and
+        //    len is the value of len.
+        A = new Array(len);
+
+        // 7. Let k be 0
+        k = 0;
+
+        // 8. Repeat, while k < len
+        while (k < len) {
+
+          var kValue, mappedValue;
+
+          // a. Let Pk be ToString(k).
+          //   This is implicit for LHS operands of the in operator
+          // b. Let kPresent be the result of calling the HasProperty internal
+          //    method of O with argument Pk.
+          //   This step can be combined with c
+          // c. If kPresent is true, then
+          if (k in O) {
+
+            // i. Let kValue be the result of calling the Get internal
+            //    method of O with argument Pk.
+            kValue = O[k];
+
+            // ii. Let mappedValue be the result of calling the Call internal
+            //     method of callback with T as the this value and argument
+            //     list containing kValue, k, and O.
+            mappedValue = callback.call(T, kValue, k, O);
+
+            // iii. Call the DefineOwnProperty internal method of A with arguments
+            // Pk, Property Descriptor
+            // { Value: mappedValue,
+            //   Writable: true,
+            //   Enumerable: true,
+            //   Configurable: true },
+            // and false.
+
+            // In browsers that support Object.defineProperty, use the following:
+            // Object.defineProperty(A, k, {
+            //   value: mappedValue,
+            //   writable: true,
+            //   enumerable: true,
+            //   configurable: true
+            // });
+
+            // For best browser support, use the following:
+            A[k] = mappedValue;
+          }
+          // d. Increase k by 1.
+          k++;
+        }
+
+        // 9. return A
+        return A;
+      };
+    }
+
+    if (!Array.prototype.filter) {
+      Array.prototype.filter = function(fun/*, thisArg*/) {
+        'use strict';
+
+        if (this === void 0 || this === null) {
+          throw new TypeError();
+        }
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== 'function') {
+          throw new TypeError();
+        }
+
+        var res = [];
+        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (var i = 0; i < len; i++) {
+          if (i in t) {
+            var val = t[i];
+
+            // NOTE: Technically this should Object.defineProperty at
+            //       the next index, as push can be affected by
+            //       properties on Object.prototype and Array.prototype.
+            //       But that method's new, and collisions should be
+            //       rare, so use the more-compatible alternative.
+            if (fun.call(thisArg, val, i, t)) {
+              res.push(val);
+            }
+          }
+        }
+
+        return res;
+      };
+    }
+
+  // Шаги алгоритма ECMA-262, 5-е издание, 15.4.4.18
+  // Ссылка (en): http://es5.github.io/#x15.4.4.18
+  // Ссылка (ru): http://es5.javascript.ru/x15.4.html#x15.4.4.18
+  if (!Array.prototype.forEach) {
+
+    Array.prototype.forEach = function (callback, thisArg) {
+
+      var T, k;
+
+      if (this == null) {
+        throw new TypeError(' this is null or not defined');
+      }
+
+      // 1. Положим O равным результату вызова ToObject passing the |this| value as the argument.
+      var O = Object(this);
+
+      // 2. Положим lenValue равным результату вызова внутреннего метода Get объекта O с аргументом "length".
+      // 3. Положим len равным ToUint32(lenValue).
+      var len = O.length >>> 0;
+
+      // 4. Если IsCallable(callback) равен false, выкинем исключение TypeError.
+      // Смотрите: http://es5.github.com/#x9.11
+      if (typeof callback !== 'function') {
+          throw new TypeError(callback + ' is not a function');
+      }
+
+      // 5. Если thisArg присутствует, положим T равным thisArg; иначе положим T равным undefined.
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+
+      // 6. Положим k равным 0
+      k = 0;
+
+      // 7. Пока k < len, будем повторять
+      while (k < len) {
+
+        var kValue;
+
+        // a. Положим Pk равным ToString(k).
+        //   Это неявное преобразование для левостороннего операнда в операторе in
+        // b. Положим kPresent равным результату вызова внутреннего метода HasProperty объекта O с аргументом Pk.
+        //   Этот шаг может быть объединён с шагом c
+        // c. Если kPresent равен true, то
+        if (k in O) {
+
+          // i. Положим kValue равным результату вызова внутреннего метода Get объекта O с аргументом Pk.
+          kValue = O[k];
+
+          // ii. Вызовем внутренний метод Call функции callback с объектом T в качестве значения this и
+          // списком аргументов, содержащим kValue, k и O.
+          callback.call(T, kValue, k, O);
+        }
+        // d. Увеличим k на 1.
+        k++;
+      }
+      // 8. Вернём undefined.
+    };
+  }
+
   function d3_class(ctor, properties) {
     try {
       for (var key in properties) {

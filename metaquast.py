e86f2fd8198cf1e0a8fda70ab17f024fcbb6c47a
Alexey Gurevich <alexeigurevich@gmail.com>
2016-04-12 18:11:39 +0300

    Better handling existing/non-existing cov and sv-bed files

--- metaquast.py
+++ metaquast.py
@@ -537,7 +537,7 @@ def main(args):
             reads_fpath_r = arg
             quast_py_args = __remove_from_quast_py_args(quast_py_args, opt, arg)
         elif opt == '--sv-bed':
-            bed_fpath = arg
+            bed_fpath = assert_file_exists(arg, 'BED with structural variations')
         else:
             logger.error('Unknown option: %s. Use -h for help.' % (opt + ' ' + arg), to_stderr=True, exit_with_code=2)
 

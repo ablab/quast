ee0f15f57b5565a393dde03a5e2086f59ef98923
Alexey Gurevich <alexeigurevich@gmail.com>
2016-04-15 16:13:57 +0300

    Full install -- reordering tests, so first perform ./install.sh and than advanced features

--- install_full.sh
+++ install_full.sh
@@ -22,16 +22,6 @@ if [ $return_code -ne 0 ]; then
    echo 'ERROR! QUAST TEST FAILED!'
    exit 1
 fi
-echo "Starting QUAST test with structural variants detection... (stdout redirected to $stdout_log_fname)"
-echo "" > $stdout_log_fname
-echo "Starting QUAST test with structural variants detection" >> $stdout_log_fname
-$quast_home/quast.py --test-sv >> $stdout_log_fname
-return_code=$?
-if [ $return_code -ne 0 ]; then
-   echo 'ERROR! QUAST TEST WITH STRUCTURAL VARIANTS DETECTION FAILED!'
-   echo 'You can try to install lightweight version of QUAST with ./install.sh'
-   exit 1
-fi
 echo "Starting MetaQUAST test... (stdout redirected to $stdout_log_fname)"
 echo "" >> $stdout_log_fname
 echo "Starting MetaQUAST test" >> $stdout_log_fname
@@ -41,6 +31,16 @@ if [ $return_code -ne 0 ]; then
    echo 'ERROR! METAQUAST TEST FAILED!'
    exit 1
 fi
+echo "Starting QUAST test with structural variants detection... (stdout redirected to $stdout_log_fname)"
+echo "" >> $stdout_log_fname
+echo "Starting QUAST test with structural variants detection" >> $stdout_log_fname
+$quast_home/quast.py --test-sv >> $stdout_log_fname
+return_code=$?
+if [ $return_code -ne 0 ]; then
+   echo 'ERROR! QUAST TEST WITH STRUCTURAL VARIANTS DETECTION FAILED!'
+   echo 'However, lightweight version of QUAST was installed successfully!'
+   exit 1
+fi
 echo "Starting MetaQUAST test without references... (stdout redirected to $stdout_log_fname)"
 echo "" >> $stdout_log_fname
 echo "Starting MetaQUAST test without references" >> $stdout_log_fname
@@ -48,7 +48,7 @@ $quast_home/metaquast.py --test-no-ref --fast >> $stdout_log_fname
 return_code=$?
 if [ $return_code -ne 0 ]; then
    echo 'ERROR! METAQUAST TEST WITHOUT REFERENCES FAILED!'
-   echo 'You can try to install lightweight version of QUAST with ./install.sh'
+   echo 'However, lightweight version of QUAST was installed successfully!'
    exit 1
 fi
 echo 'QUAST INSTALLED SUCCESSFULLY!'

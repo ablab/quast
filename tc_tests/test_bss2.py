9df29044f94ca0427bf821b51cf939c4d5f970a6
Alexey Gurevich <alexeigurevich@gmail.com>
2016-04-29 19:04:40 +0300

    TeamCity: advanced test of BSS

---
+++ tc_tests/test_bss2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from __future__ import with_statement
+import os
+from common import *
+
+name = os.path.basename(__file__)[5:-3]
+
+# bss for Best Set Selection
+run_quast(name, contigs=['scaffolds_with_many_repeats.fa.gz'], params=' -R ref_with_many_repeats.fa.gz --fast')
+
+assert_metric(name, '# misassemblies', ['17'], 'report.tsv')
+assert_metric(name, '# misassembled contigs', ['9'], 'report.tsv')
+assert_metric(name, '# local misassemblies', ['182'], 'report.tsv')

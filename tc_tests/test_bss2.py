e875dc56ba0fd18ea4e5c065366f3e37251d2983
Alexey Gurevich <alexeigurevich@gmail.com>
2016-07-13 14:19:30 +0300

    TeamCity: BSS2 test updated after changes in BSS algorithm

--- tc_tests/test_bss2.py
+++ tc_tests/test_bss2.py
@@ -9,6 +9,6 @@ name = os.path.basename(__file__)[5:-3]
 # bss for Best Set Selection
 run_quast(name, contigs=['scaffolds_with_many_repeats.fa.gz'], params=' -R ref_with_many_repeats.fa.gz --fast')
 
-assert_metric(name, '# misassemblies', ['18'], 'report.tsv')
-assert_metric(name, '# misassembled contigs', ['10'], 'report.tsv')
+assert_metric(name, '# misassemblies', ['17'], 'report.tsv')
+assert_metric(name, '# misassembled contigs', ['9'], 'report.tsv')
 assert_metric(name, '# local misassemblies', ['182'], 'report.tsv')

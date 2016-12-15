bbecd232f1bdbd4bbfe7df00b0599b68d4f709fe
vladsaveliev <vladsaveliev@me.com>
2016-12-15 03:47:16 +0300

    Fix logger.warn -> logger.warning

--- quast.py
+++ quast.py
@@ -48,7 +48,7 @@ def main(args):
         try:
             locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
         except Exception:
-            logger.warn('Python locale settings can\'t be changed')
+            logger.warning('Python locale settings can\'t be changed')
     quast_path = [os.path.realpath(__file__)]
     quast_py_args, contigs_fpaths = parse_options(logger, quast_path + args)
     output_dirpath, ref_fpath, labels = qconfig.output_dirpath, qconfig.reference, qconfig.labels

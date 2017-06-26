58fb1592177f3b6c34c1923db9602506b17faab3
almiheenko <al.miheenko@gmail.com>
2017-06-26 20:37:23 +0300

    TC tests: fix BSS2

--- tc_tests/test_bss2.py
+++ tc_tests/test_bss2.py
@@ -10,6 +10,6 @@ name = os.path.basename(__file__)[5:-3]
 run_quast(name, contigs=['scaffolds_with_many_repeats.fa.gz'], params=' -R ref_with_many_repeats.fa.gz --fast')
 
 assert_metric(name, '# misassemblies', ['7'], 'report.tsv')
-assert_metric(name, '# scaffold gap size misassemblies', ['10'], 'report.tsv')
+assert_metric(name, '# scaffold gap size misassemblies', ['12'], 'report.tsv')
 assert_metric(name, '# misassembled contigs', ['4'], 'report.tsv')
-assert_metric(name, '# local misassemblies', ['182'], 'report.tsv')
+assert_metric(name, '# local misassemblies', ['180'], 'report.tsv')

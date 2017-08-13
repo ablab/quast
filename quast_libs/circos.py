6f27ad264809a30d978d9f2a657f07858033cb4d
almiheenko <al.miheenko@gmail.com>
2017-08-13 13:50:47 +0400

    Circos: tiny python2.5 fix

--- quast_libs/circos.py
+++ quast_libs/circos.py
@@ -1,3 +1,12 @@
+############################################################################
+# Copyright (c) 2015-2017 Saint Petersburg State University
+# Copyright (c) 2011-2015 Saint Petersburg Academic University
+# All Rights Reserved
+# See file LICENSE for details.
+############################################################################
+
+from __future__ import with_statement
+
 import os
 from collections import defaultdict
 from os.path import join, exists

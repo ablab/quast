49ffe91e7d7cf4559c59f24c59d059b4dbfc10e3
almiheenko <al.miheenko@gmail.com>
2017-08-13 13:40:52 +0400

    Fixed BSS2 TC test

--- tc_tests/test_bss2.py
+++ tc_tests/test_bss2.py
@@ -9,7 +9,7 @@ name = os.path.basename(__file__)[5:-3]
 # bss for Best Set Selection
 run_quast(name, contigs=['scaffolds_with_many_repeats.fa.gz'], params=' -R ref_with_many_repeats.fa.gz --fast')
 
-assert_metric(name, '# misassemblies', ['7'], 'report.tsv')
-assert_metric(name, '# scaffold gap size misassemblies', ['12'], 'report.tsv')
-assert_metric(name, '# misassembled contigs', ['4'], 'report.tsv')
-assert_metric(name, '# local misassemblies', ['180'], 'report.tsv')
+assert_metric(name, '# misassemblies', ['8'], 'report.tsv')
+assert_metric(name, '# scaffold gap size misassemblies', ['9'], 'report.tsv')
+assert_metric(name, '# misassembled contigs', ['5'], 'report.tsv')
+assert_metric(name, '# local misassemblies', ['182'], 'report.tsv')

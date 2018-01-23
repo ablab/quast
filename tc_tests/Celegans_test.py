a4edfd50e79fc1690b6e05ab26388b9a21459081
Alexey Gurevich <alexeigurevich@gmail.com>
2018-01-23 17:01:14 +0300

    Tests: fixed Celegans tests

--- tc_tests/Celegans_test.py
+++ tc_tests/Celegans_test.py
@@ -12,11 +12,11 @@ run_quast(name, contigs=['/Johnny/data/contigs/C.elegans/PROBABLE_CONTIGS/abyss.
                          '/Johnny/data/contigs/C.elegans/PROBABLE_CONTIGS/velvet.fa',
                          '/Johnny/data/contigs/C.elegans/SPAdes2.4.scaffolds.fasta'],
                  params=' -R /Johnny/data/contigs/C.elegans/REF_FILES/reference.fasta '
-                        '-t 8 --no-plots')
+                        '-t 8 --no-plots --large')
 
 check_report_files(name, ['icarus.html',
                           'genome_stats/genome_info.txt'])
 
 assert_metric(name, '# contigs (>= 50000 bp)', ['274', '219', '263', '148', '243'], 'report.tsv')
-assert_metric(name, '# misassemblies', ['151', '52', '82', '196', '322'], 'report.tsv')
+assert_metric(name, '# misassemblies', ['95', '19', '22', '100', '183'], 'report.tsv')
 assert_metric(name, 'Largest alignment', ['202579', '187587', '147763', '168091', '172159'], 'report.tsv')

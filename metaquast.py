f1a094bd54407d38e6603e7740028cdf47a47bae
Alexey Gurevich <alexeigurevich@gmail.com>
2018-06-21 19:10:41 +0300

    Copyrights update (Happy [Australian] New Year!)

--- metaquast.py
+++ metaquast.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ############################################################################
-# Copyright (c) 2015-2017 Saint Petersburg State University
+# Copyright (c) 2015-2018 Saint Petersburg State University
 # Copyright (c) 2011-2015 Saint Petersburg Academic University
 # All Rights Reserved
 # See file LICENSE for details.

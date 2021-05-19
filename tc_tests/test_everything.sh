2023ea6de9db1dba7e8e6ba9b4ce41e6b3367bf0
Alexey Gurevich <alexeigurevich@gmail.com>
2021-05-19 18:57:06 +0300

    Tests: changes in test_everything.sh to allow it running from any dir

--- tc_tests/test_everything.sh
+++ tc_tests/test_everything.sh
@@ -3,14 +3,18 @@
 if [ $# -gt 0 ]; then
     python_interpreter=$1
 else
-    python_interpreter=python2.6
+    python_interpreter=python
 fi
 
-sh ../clean.sh
+# based on https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+CWD=`pwd`
+cd $SCRIPT_DIR
 for f in test_*.py; do
     $python_interpreter "$f";
     if [ ! $? -eq 0 ]; then exit 1; fi;
     echo;
     echo;
-    done
+done
+cd $CWD

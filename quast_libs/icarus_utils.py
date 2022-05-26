f5d711677f5ae18db3a3a4a83ae20bd70f6173e3
Alexey Gurevich <alexeigurevich@gmail.com>
2022-05-26 17:05:37 +0300

    cosmetic: Welcome 2022!

--- quast_libs/icarus_utils.py
+++ quast_libs/icarus_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -O
 
 ############################################################################
-# Copyright (c) 2015-2020 Saint Petersburg State University
+# Copyright (c) 2015-2022 Saint Petersburg State University
 # Copyright (c) 2011-2015 Saint Petersburg Academic University
 # All Rights Reserved
 # See file LICENSE for details.

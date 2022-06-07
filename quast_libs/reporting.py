efc98abb76244293da326bf86cc6e1ec48fe9fab
Alexey Gurevich <alexeigurevich@gmail.com>
2022-06-07 13:12:29 +0300

    reporting: do not show NAx-like metrics if nothing aligned at all

--- quast_libs/reporting.py
+++ quast_libs/reporting.py
@@ -454,6 +454,26 @@ def table(order=Fields.order, ref_name=None):
         order = [('', order)]
 
     table = []
+    required_fields = []
+
+    def define_required_fields():
+        # if a reference is specified, keep the same number of Nx/Lx-like genome-based metrics in different reports
+        # (no matter what percent of the genome was assembled)
+        report = get(assembly_fpaths[0], ref_name=ref_name)
+        if report.get_field(Fields.REFLEN):
+            anyone_aligned = False
+            for assembly_fpath in assembly_fpaths:
+                report = get(assembly_fpath, ref_name=ref_name)
+                if report.get_field(Fields.MAPPEDGENOME):
+                    anyone_aligned = True
+                    break
+
+            if anyone_aligned:
+                required_fields.extend([Fields.NA50, Fields.LA50, Fields.NAx, Fields.LAx])
+            if not qconfig.is_combined_ref:
+                required_fields.extend([Fields.NG50, Fields.LG50, Fields.NGx, Fields.LGx])
+                if anyone_aligned:
+                    required_fields.extend([Fields.NGA50, Fields.LGA50, Fields.NGAx, Fields.LGAx])
 
     def append_line(rows, field, are_multiple_thresholds=False, pattern=None, feature=None, i=None, ref_name=None):
         quality = get_quality(field)
@@ -468,11 +488,6 @@ def table(order=Fields.order, ref_name=None):
             else:
                 values.append(value)
 
-        required_fields = []
-        if report.get_field(Fields.REFLEN):  # keep the same number of metrics in different reports (no matter what percent of the genome was assembled)
-            required_fields = [Fields.NA50, Fields.LA50, Fields.NAx, Fields.LAx]
-            if not qconfig.is_combined_ref:
-                required_fields.extend([Fields.NG50, Fields.NGA50, Fields.LG50, Fields.LGA50, Fields.NGx, Fields.NGAx, Fields.LGx, Fields.LGAx])
         if list(filter(lambda v: v is not None, values)) or field in required_fields:
             metric_name = field if (feature is None) else pattern % int(feature)
             # ATTENTION! Contents numeric values, needed to be converted to strings.
@@ -483,6 +498,7 @@ def table(order=Fields.order, ref_name=None):
                 'isMain': metric_name in Fields.main_metrics,
             })
 
+    define_required_fields()
     for group_name, metrics in order:
         rows = []
         table.append((group_name, rows))
